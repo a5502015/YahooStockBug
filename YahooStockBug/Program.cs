1493bbc0198d6bfcb38d4e22daeca3506a65d325
F528 <a5522008@gmail.com>
2019-04-19 10:24:08 +0800

    加入專案檔案。

---
+++ YahooStockBug/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace YahooStockBug
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = @"https://tw.stock.yahoo.com/us/worldidx.php";
+            Stock st = new Stock(url);
+
+            //string ans = await.getUrlResponAsync();
+            string ans = await st.getUrlResponAsync();
+
+            Console.WriteLine(ans);
+
+            using (StreamWriter sw = new StreamWriter("./page.html", false))
+            {
+                 sw.Write(ans);
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ YahooStockBug/Stock.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+
+namespace YahooStockBug
+{
+    class Stock
+    {
+        private string url;
+        public Stock(string link)
+        {
+            string rule = @"/^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/";
+            if (new Regex(rule).IsMatch(link))
+            {
+                url = link;
+            }
+            else
+            {
+                url = @"https://tw.stock.yahoo.com/us/worldidx.php";
+            }
+        }
+        public async Task<string> getUrlResponAsync()
+        {
+            string html = "";
+            using (HttpClient client = new HttpClient())
+            {
+                // Call asynchronous network methods in a try/catch block to handle exceptions
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    // Above three lines can be replaced with new helper method below
+                    // string responseBody = await client.GetStringAsync(uri);
+                    html = responseBody;
+                    Console.WriteLine(responseBody);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+                }
+            }
+
+            return html;
+        }
+
+    }
+}
